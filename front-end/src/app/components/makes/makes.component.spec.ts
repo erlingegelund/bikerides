f7e040bbd44ac21e43c69aef77f92c4bffaf02e1
Erling Friis Egelund <erling.f.egelund@gmail.com>
2024-08-26 20:54:13 +0200

    components for models

---
+++ front-end/src/app/components/makes/makes.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MakesComponent } from './makes.component';
+
+describe('MakesComponent', () => {
+  let component: MakesComponent;
+  let fixture: ComponentFixture<MakesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MakesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MakesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front-end/src/app/components/makes/makes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-makes',
+  standalone: true,
+  imports: [],
+  templateUrl: './makes.component.html',
+  styleUrl: './makes.component.scss'
+})
+export class MakesComponent {
+
+}
